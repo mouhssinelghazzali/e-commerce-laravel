52a058a11fd092ac9d49005af6db471444eaf20f
mouhssine elghazzali <mouhssine.elghazzali@gmail.com>
2020-05-13 04:23:48 +0200

    total et tax panier

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -80,11 +80,11 @@
                     <div class="p-4">
                         <p class="font-italic mb-4">Shipping and additional costs are calculated based on values you have entered.</p>
                         <ul class="list-unstyled mb-4">
-                        <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">sous-total </strong><strong>{{ Cart::subtotal() }}</strong></li>
+                        <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">sous-total </strong><strong>{{ getPrice(Cart::subtotal()) }}</strong></li>
                         <!-- <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Shipping and handling</strong><strong>$10.00</strong></li> -->
-                        <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Tax</strong><strong>$0.00</strong></li>
+                        <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Taxe</strong><strong>{{ getPrice(Cart::tax()) }}</strong></li>
                         <li class="d-flex justify-content-between py-3 border-bottom"><strong class="text-muted">Total</strong>
-                            <h5 class="font-weight-bold">$400.00</h5>
+                            <h5 class="font-weight-bold">{{ getPrice(Cart::total()) }}</h5>
                         </li>
                         </ul><a href="#" class="btn btn-dark rounded-pill py-2 btn-block">Procceed to checkout</a>
                     </div>
